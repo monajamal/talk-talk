16576e9c25d93043ef9b09e194525951cd81ebc9
mariehelenegoddet <mariehelenegoddet@437ad852-65f7-82e2-17ed-af48104ca5f9>
2011-02-24 12:34:09 +0000

    Gère le nat...

--- trunk/Talk-Talk/src/talkTalk/DistantImpl.java
+++ trunk/Talk-Talk/src/talkTalk/DistantImpl.java
@@ -1,25 +1,18 @@
 package talkTalk;
 
 import java.rmi.RemoteException;
-import java.rmi.server.UnicastRemoteObject;
 import java.util.Vector;
 
-@SuppressWarnings("serial")
-public class DistantImpl extends UnicastRemoteObject implements Distant {
+// extends UnicastRemoteObject
+public class DistantImpl implements Distant {
+	
+	private static final long serialVersionUID = -4154160384111399024L;
 	public DistantImpl() throws RemoteException {
 		super();
 	}
 
 	@Override
 	public boolean sendMsg(String expediteur, String msg) throws RemoteException {
-		//InetAddress addr;
-		//String hostName = null;
-		/*try {
-			addr = InetAddress.getLocalHost();
-			hostName = new String(addr.getHostName());
-		} catch (UnknownHostException e) {
-			e.printStackTrace();
-		} */
 		System.out.println(expediteur+" : "+msg);
 		return true;
 	}
--- trunk/Talk-Talk/src/talkTalk/TalkTalk.java
+++ trunk/Talk-Talk/src/talkTalk/TalkTalk.java
@@ -6,12 +6,15 @@ import java.net.NetworkInterface;
 import java.net.SocketException;
 import java.net.UnknownHostException;
 import java.rmi.Naming;
+import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
+import java.rmi.server.UnicastRemoteObject;
 import java.util.Enumeration;
 import java.util.Hashtable;
 import java.util.List;
 import java.util.Properties;
+
 import utils.SaisieControle;
 
 public class TalkTalk {
@@ -21,15 +24,18 @@ public class TalkTalk {
 	public static String image;
 	public static String statut;
 	public static Hashtable<String,Contact> friends;
+	public final static int portObject = 3000;
 	
 	public static void main(String[] args) throws UnknownHostException {
 		if (args.length==1) pseudo=args[0]; else pseudo="namelessTalk";
 
+		//Pour choisir l'interface par defaut
 		//InetAddress addr = InetAddress.getLocalHost();
 		//hostname = new String(addr.getHostAddress());
 		
-		//On cherche une interface correcte
+		//On cherche une interface correcte (plus sûr)
 		Enumeration<NetworkInterface> interfaces = null;
+		InetAddress addr = null;
 		try {
 			interfaces = NetworkInterface.getNetworkInterfaces();
 			while (interfaces.hasMoreElements()) {  // carte reseau trouvee
@@ -40,10 +46,10 @@ public class TalkTalk {
 					String adress = ia.getHostAddress().toString();
 					//System.out.println("Adresse : "+adress);
 					if( adress.length() < 16){          //On s'assure ainsi que l'adresse IP est bien IPv4
-						if(adress.startsWith("127")){  //Ce n'est pas l'adresse IP Local' 
+						if(adress.startsWith("127")){  //C'est localhost
 							//System.out.println("127 " +ia.getHostAddress());
 						} else {
-							hostname = ia.getHostAddress();
+							addr = ia;
 						}
 					}
 					
@@ -53,10 +59,18 @@ public class TalkTalk {
 		} catch (SocketException e1) {
 			e1.printStackTrace();
 		}
-
+		
+		if (addr == null) {
+			System.out.println("Etes vous bien connecte quelque part ?");
+			System.exit(-1);
+		}
+		//Nom de la machine
+		hostname = addr.getHostName();
 		// Rmi doit ecouter sur la bonne interface
 		Properties p = System.getProperties();
-		p.setProperty("java.rmi.server.hostname",hostname);
+		p.setProperty("java.rmi.server.hostname",addr.getHostAddress());
+		//On remplace par l'adresse ip publique si on veut utiliser du nat
+		//p.setProperty("java.rmi.server.hostname","stggw1.homeip.net");
 		
 		image=null;
 		statut="dispo";
@@ -68,16 +82,21 @@ public class TalkTalk {
 		
 		//Contact.saveContact(friends);
 		
-		// équivalent du processus rmiregistry
+		//Lancement du registry
 		try {
 			LocateRegistry.createRegistry(1099);
-		} catch (RemoteException e) {
-			e.printStackTrace();
+		} catch (RemoteException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
 		}
+		
 		// Lancement du serveur
 		try {
 			DistantImpl objLocal = new DistantImpl();
-			Naming.rebind("rmi://"+hostname+":1099/TalkTalk", objLocal);
+			Remote stub = UnicastRemoteObject.exportObject(objLocal,portObject);
+			Naming.rebind("rmi://"+hostname+"/TalkTalk", stub);
+			//Si on veut utiliser du nat, il faut remplacer hostname par l'ip publique
+			//Naming.rebind("rmi://stggw1.homeip.net:1099/TalkTalk", objLocal);
 			System.out.println("SERVEUR ["+hostname+"] : Server ready !");
 		} catch (RemoteException e) {
 			e.printStackTrace();
@@ -85,13 +104,13 @@ public class TalkTalk {
 			e.printStackTrace();
 		}
 		
-		
-		
+		//Debut de l'ecoute de la console
 		String saisie = "";
 		while (saisie!=null) {
 			saisie = SaisieControle.saisieString("Commande ?");
 			if (saisie.contains("/quit")) {
 				saisie=null;
+				
 				System.out.println("SERVEUR ["+hostname+"] : Server down !");
 				System.exit(0);
 			} else if (saisie.contains("/add")) {
@@ -113,22 +132,8 @@ public class TalkTalk {
 					Envoi env = new Envoi(saisie.substring(0, saisie.indexOf(' ')),saisie.substring(saisie.indexOf(' ')+1));
 					env.start();
 				}
-				/*Distant b;
-				try {
-					System.out.println("moi: "+saisie);
-					for (int i=0;i<friend.size();i++) {
-						b = (Distant)Naming.lookup("rmi://"+friend.get(i)+"/TalkTalk");
-						b.sendMsg(pseudo,saisie);
-					}
-				} catch (MalformedURLException e) {
-					e.printStackTrace();
-				} catch (RemoteException e) {
-					e.printStackTrace();
-				} catch (NotBoundException e) {
-					e.printStackTrace();
-				}*/
 			} else {
-				
+				System.out.println("Commande inconnue");
 			}
 		}
 	}
@@ -151,13 +156,6 @@ public class TalkTalk {
 		}
 		res+=" }";
 		return res;
-		/*String res="{ ";
-		for (int i=0;i<friend.size();i++) {
-			if (i!=0) res+=", ";
-			res+=friend.get(i);
-		}
-		res+=" }";
-		return res;*/
 	}
 }
 //Debug.start(args);
